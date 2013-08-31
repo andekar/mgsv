89caa66b8910a11b7332f074914eab0dd29d2218
Anders <andersk84@gmail.com>
2013-08-31 16:33:50 +0200

    add validate user file

---
+++ apps/mgsv/src/validate_user.erl
@@ -0,0 +1,24 @@
+-module(validate_user).
+
+-include("payapp.hrl").
+
+-export([validate/2]).
+
+-define(GOOGLE_URL, "https://www.googleapis.com/oauth2/v1/userinfo?access_token=").
+-define(EMAIL, <<"email">>).
+
+validate(Token, ?GMAIL_USER) ->
+    validate_google(binary_to_list(Token));
+validate(_Token, _) ->
+    undefined.
+
+validate_google(Token) ->
+    Method = get,
+    URL = ?GOOGLE_URL ++ Token,
+    Header = [],
+    HTTPOptions = [],
+    Options = [],
+    {ok, {{"HTTP/1.1",_ReturnCode, _State}, _Head, Body}}
+        = httpc:request(Method, {URL, Header}, HTTPOptions, Options),
+    {struct, List} = mochijson2:decode(Body),
+    proplists:get_value(?EMAIL, List).
