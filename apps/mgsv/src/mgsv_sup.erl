3dc6b6bd18cd47d2361134f64a31e78d531d5760
Anders <andersk84@gmail.com>
2017-11-05 14:22:55 +0100

    mgsv_sup: fix supervisor starts

--- apps/mgsv/src/mgsv_sup.erl
+++ apps/mgsv/src/mgsv_sup.erl
@@ -24,11 +24,14 @@ start_link() ->
 
 
 init([]) ->
-%    Lager = ?CHILD(mgsv_lager_sup, supervisor),
     MgsvServer = ?CHILD(mgsv_server, worker),
     WebMachine = ?CHILD(machine_sup, supervisor),
     PayServer = ?CHILD(pay_server, worker),
     PayPushNotification = ?CHILD(pay_push_notification, worker),
     ValidateUsers = ?CHILD(validate_user, worker),
-    Processes = [WebMachine, MgsvServer, PayServer, PayPushNotification, ValidateUsers],
+    Processes = [WebMachine
+                , MgsvServer
+                , PayServer
+                , PayPushNotification
+                , ValidateUsers],
     {ok, { {one_for_one, 6000, 1}, Processes} }.
